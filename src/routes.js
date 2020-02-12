2522d711d1cfb7affb23f5a09df46f50bf2d2565
Henrique Derosa <henriquederosa@Henriques-MacBook-Pro.local>
2020-02-11 21:05:42 -0300

    add auth middleware

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,13 @@ import jwt from 'jsonwebtoken'
 import user from './app/controllers/UserController'
 import SessionController from './app/controllers/SessionController'
 
+import authMiddleware from './app/middlewares/auth'
+
 const routes = new Router()
 
 routes.post('/login', SessionController.store)
+routes.use(authMiddleware)
+
 routes.get('/user', user.index)
 routes.post('/user', user.store)
 
